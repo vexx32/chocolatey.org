0e5a2072a5225d84b33894a45f42281d8989ffb0
Rob Reynolds <ferventcoder@gmail.com>
2015-04-03 20:51:53 -0500

    (maint) formatting

--- chocolatey/Website/Install.ps1
+++ chocolatey/Website/Install.ps1
@@ -1,18 +1,18 @@
 # ==============================================================================
-# 
-# Fervent Coder Copyright 2011 - Present - Released under the Apache 2.0 License
-# 
-# Copyright 2007-2008 The Apache Software Foundation.
-#  
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
-# this file except in compliance with the License. You may obtain a copy of the 
-# License at 
 #
-#     http://www.apache.org/licenses/LICENSE-2.0 
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+# Fervent Coder Copyright 2011 - Present - Released under the Apache 2.0 License
+#
+# Copyright 2007-2008 The Apache Software Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 # ==============================================================================
 
@@ -47,7 +47,7 @@ $7zaExe = Join-Path $tempDir '7za.exe'
 Download-File 'https://chocolatey.org/7za.exe' "$7zaExe"
 
 # Github's Raw endpoint does not honor TLS and the .net 2.0 client will
-# not fall back to Ssl3 un like the newer .net4 clients. So .net2 will 
+# not fall back to Ssl3 un like the newer .net4 clients. So .net2 will
 # time out if we do not explicitly set the protocol to Ssl3
 #$currentProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 #try {
@@ -64,9 +64,9 @@ Download-File 'https://chocolatey.org/7za.exe' "$7zaExe"
 # unzip the package
 Write-Host "Extracting $file to $tempDir..."
 Start-Process "$7zaExe" -ArgumentList "x -o`"$tempDir`" -y `"$file`"" -Wait -NoNewWindow
-#$shellApplication = new-object -com shell.application 
-#$zipPackage = $shellApplication.NameSpace($file) 
-#$destinationFolder = $shellApplication.NameSpace($tempDir) 
+#$shellApplication = new-object -com shell.application
+#$zipPackage = $shellApplication.NameSpace($file)
+#$destinationFolder = $shellApplication.NameSpace($tempDir)
 #$destinationFolder.CopyHere($zipPackage.Items(),0x10)
 
 # call chocolatey install
