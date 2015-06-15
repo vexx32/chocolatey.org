e162d82d3e6a0e8bbe45e290f6515f3cfbd71e29
Rob Reynolds <ferventcoder@gmail.com>
2015-06-15 01:28:52 -0500

    (GH-196) Show nuspec reference
    When hovering over items on the package page, show the nuspec
    reference. In most locations show it as it would be specified in the
    actual nuspec.

--- nugetgallery/Website/ViewModels/DependencySetsViewModel.cs
+++ nugetgallery/Website/ViewModels/DependencySetsViewModel.cs
@@ -16,8 +16,8 @@ namespace NuGetGallery
                 .OrderBy(ds => ds.Key);
 
             OnlyHasAllFrameworks = dependencySets.Count() == 1 && dependencySets.First().Key == null;
-            
-            foreach(var dependencySet in dependencySets)
+
+            foreach (var dependencySet in dependencySets)
             {
                 var targetFramework = dependencySet.Key == null ? "All Frameworks" : VersionUtility.ParseFrameworkName(dependencySet.Key).ToFriendlyName();
                 DependencySets.Add(targetFramework, dependencySet.Select(d => d.Id == null ? null : new DependencyViewModel(d.Id, d.VersionSpec)));
@@ -33,12 +33,17 @@ namespace NuGetGallery
             {
                 Id = id;
 
+
                 if (!String.IsNullOrEmpty(versionSpec))
+                {
+                    RawVersionSpec = versionSpec;
                     VersionSpec = VersionUtility.PrettyPrint(VersionUtility.ParseVersionSpec(versionSpec));
+                }
             }
 
             public string Id { get; private set; }
             public string VersionSpec { get; private set; }
+            public string RawVersionSpec { get; private set; }
         }
     }
 }
