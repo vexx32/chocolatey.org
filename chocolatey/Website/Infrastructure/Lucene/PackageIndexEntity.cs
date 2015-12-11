85be74c4c360a5dabb52072ae4706fe64bdb5f17
Rob Reynolds <ferventcoder@gmail.com>
2015-12-11 09:58:01 -0600

    (GH-231) Lucene Package as index
    Index all fields from the package used for the site and the OData feed.

--- chocolatey/Website/Infrastructure/Lucene/PackageIndexEntity.cs
+++ chocolatey/Website/Infrastructure/Lucene/PackageIndexEntity.cs
@@ -44,45 +44,39 @@ namespace NuGetGallery
             var document = new Document();
 
             // Note: Used to identify index records for updates
-            document.Add(
-                new Field(
-                    "PackageRegistrationKey",
-                    Package.PackageRegistrationKey.ToString(CultureInfo.InvariantCulture),
-                    Field.Store.YES,
-                    Field.Index.NOT_ANALYZED));
-
+            document.Add(new Field("PackageRegistrationKey", Package.PackageRegistrationKey.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NOT_ANALYZED));
             document.Add(new Field("Key", Package.Key.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NOT_ANALYZED));
-            
-            var field = new Field("Id-Exact", Package.PackageRegistration.Id.ToLowerInvariant(), Field.Store.NO, Field.Index.NOT_ANALYZED);
 
+            //id fields
+            document.Add(new Field("Id-Original", Package.PackageRegistration.Id, Field.Store.YES, Field.Index.NO));
+
+            var field = new Field("Id-Exact", Package.PackageRegistration.Id.ToLowerInvariant(), Field.Store.NO, Field.Index.NOT_ANALYZED);
             field.Boost = 2.5f;
             document.Add(field);
-
-            // Store description so we can show them in search results
-            field = new Field("Description", Package.Description, Field.Store.YES, Field.Index.ANALYZED);
-            field.Boost = 0.1f;
-            document.Add(field);
-
+            
             // We store the Id/Title field in multiple ways, so that it's possible to match using multiple
             // styles of search
             // Note: no matter which way we store it, it will also be processed by the Analyzer later.
 
             // Style 1: As-Is Id, no tokenizing (so you can search using dot or dash-joined terms)
             // Boost this one
-            field = new Field("Id", Package.PackageRegistration.Id, Field.Store.NO, Field.Index.ANALYZED);
+            field = new Field("Id", Package.PackageRegistration.Id.ToLowerInvariant(), Field.Store.NO, Field.Index.ANALYZED);
             document.Add(field);
 
             // Style 2: dot+dash tokenized (so you can search using undotted terms)
-            field = new Field("Id", SplitId(Package.PackageRegistration.Id), Field.Store.NO, Field.Index.ANALYZED);
+            field = new Field("Id", SplitId(Package.PackageRegistration.Id.ToLowerInvariant()), Field.Store.NO, Field.Index.ANALYZED);
             field.Boost = 0.8f;
             document.Add(field);
 
             // Style 3: camel-case tokenized (so you can search using parts of the camelCasedWord). 
             // De-boosted since matches are less likely to be meaningful
-            field = new Field("Id", CamelSplitId(Package.PackageRegistration.Id), Field.Store.NO, Field.Index.ANALYZED);
+            field = new Field("Id", CamelSplitId(Package.PackageRegistration.Id.ToLowerInvariant()), Field.Store.NO, Field.Index.ANALYZED);
             field.Boost = 0.25f;
             document.Add(field);
 
+            document.Add(new Field("Version", Package.Version.to_string(), Field.Store.YES, Field.Index.NO));
+
+            // title fields
             // If an element does not have a Title, fall back to Id, same as the website.
             var workingTitle = String.IsNullOrEmpty(Package.Title)
                                    ? Package.PackageRegistration.Id
@@ -103,21 +97,67 @@ namespace NuGetGallery
             field.Boost = 0.5f;
             document.Add(field);
 
-            if (!String.IsNullOrEmpty(Package.Tags))
-            {
-                // Store tags so we can show them in search results
-                field = new Field("Tags", Package.Tags, Field.Store.YES, Field.Index.ANALYZED);
-                field.Boost = 0.8f;
-                document.Add(field);
-            }
+            document.Add(new Field("Summary", Package.Summary.to_string(), Field.Store.YES, Field.Index.NO));
 
-            document.Add(new Field("Authors", Package.FlattenedAuthors.ToStringSafe(), Field.Store.YES, Field.Index.ANALYZED));
+            // Store description so we can show them in search results
+            field = new Field("Description", Package.Description, Field.Store.YES, Field.Index.ANALYZED);
+            field.Boost = 0.1f;
+            document.Add(field);
+
+            // Store tags so we can show them in search results
+            field = new Field("Tags", Package.Tags.to_string(), Field.Store.YES, Field.Index.ANALYZED);
+            field.Boost = 0.8f;
+            document.Add(field);
+
+            document.Add(new Field("Authors", Package.FlattenedAuthors.to_string(), Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field("Copyright", Package.Copyright.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("Created", Package.Created.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("FlattenedDependencies", Package.FlattenedDependencies.to_string(), Field.Store.YES, Field.Index.NO));
+            // sorting
+            document.Add(new Field("DownloadCount", Package.PackageRegistration.DownloadCount.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            document.Add(new Field("VersionDownloadCount", Package.DownloadCount.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
 
             // Fields for storing data to avoid hitting SQL while doing searches
-            if (!String.IsNullOrEmpty(Package.IconUrl))
+            document.Add(new Field("IconUrl", Package.IconUrl.to_string(), Field.Store.YES, Field.Index.NO));
+
+            // Fields meant for filtering, also storing data to avoid hitting SQL while doing searches
+            document.Add(new Field("IsLatest", Package.IsLatest.to_string(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            document.Add(new Field("IsLatestStable", Package.IsLatestStable.to_string(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            document.Add(new Field("IsPrerelease", Package.IsPrerelease.to_string(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+
+            document.Add(new Field("Language", Package.Language.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("LastUpdated", Package.LastUpdated.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("Published", Package.Published.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            // Fields meant for filtering, sorting
+            document.Add(new Field("PublishedDate", Package.Published.Ticks.ToString(CultureInfo.InvariantCulture), Field.Store.NO, Field.Index.NOT_ANALYZED));
+            document.Add(new Field("LicenseUrl", Package.LicenseUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("RequiresLicenseAcceptance", Package.RequiresLicenseAcceptance.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("Hash", Package.Hash.ToStringSafe(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("HashAlgorithm", Package.HashAlgorithm.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageFileSize", Package.PackageFileSize.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("ProjectUrl", Package.ProjectUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("ReleaseNotes", Package.ReleaseNotes.to_string(), Field.Store.YES, Field.Index.NO));
+
+            // nuspec enhancements
+            document.Add(new Field("ProjectSourceUrl", Package.ProjectSourceUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageSourceUrl", Package.PackageSourceUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("DocsUrl", Package.DocsUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("MailingListUrl", Package.MailingListUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("BugTrackerUrl", Package.BugTrackerUrl.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageStatus", Package.Status.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageSubmittedStatus", Package.SubmittedStatus.to_string(), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageTestResultStatus", Package.PackageTestResultStatus.to_string(), Field.Store.YES, Field.Index.NO));
+            if (Package.PackageTestResultDate.HasValue) document.Add(new Field("PackageTestResultStatusDate", Package.PackageTestResultDate.GetValueOrDefault().ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            document.Add(new Field("PackageValidationResultStatus", Package.PackageValidationResultStatus.to_string(), Field.Store.YES, Field.Index.NO));
+            if (Package.PackageValidationResultDate.HasValue) document.Add(new Field("PackageValidationResultDate", Package.PackageValidationResultDate.GetValueOrDefault().ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            if (Package.ReviewedDate.HasValue) document.Add(new Field("PackageReviewedDate", Package.ReviewedDate.GetValueOrDefault().ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            if (Package.ApprovedDate.HasValue) document.Add(new Field("PackageApprovedDate", Package.ApprovedDate.GetValueOrDefault().ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
+            var reviewedByUserName = string.Empty;
+            if (Package.ReviewedBy != null)
             {
-                document.Add(new Field("IconUrl", Package.IconUrl, Field.Store.YES, Field.Index.NO));
+                reviewedByUserName = Package.ReviewedBy.Username;  
             }
+            document.Add(new Field("PackageReviewer", reviewedByUserName, Field.Store.YES, Field.Index.NO));
 
             if (Package.PackageRegistration.Owners.AnySafe())
             {
@@ -126,38 +166,16 @@ namespace NuGetGallery
                 document.Add(new Field("FlattenedOwners", flattenedOwners, Field.Store.YES, Field.Index.NO));
             }
 
-            document.Add(new Field("Copyright", Package.Copyright.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Created", Package.Created.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("FlattenedDependencies", Package.FlattenedDependencies.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Hash", Package.Hash.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("HashAlgorithm", Package.HashAlgorithm.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Id-Original", Package.PackageRegistration.Id, Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("LastUpdated", Package.LastUpdated.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
-         
-
-            document.Add(new Field("Language", Package.Language.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("LicenseUrl", Package.LicenseUrl.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Version", Package.Version.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-
-            document.Add(new Field("VersionDownloadCount", Package.DownloadCount.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("PackageFileSize", Package.PackageFileSize.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("ProjectUrl", Package.ProjectUrl.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Published", Package.Published.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("ReleaseNotes", Package.ReleaseNotes.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("RequiresLicenseAcceptance", Package.RequiresLicenseAcceptance.ToString(), Field.Store.YES, Field.Index.NO));
-            document.Add(new Field("Summary", Package.Summary.ToStringSafe(), Field.Store.YES, Field.Index.NO));
-            
-            // Fields meant for filtering, also storing data to avoid hitting SQL while doing searches
-            document.Add(new Field("IsLatest", Package.IsLatest.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-            document.Add(new Field("IsLatestStable", Package.IsLatestStable.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-
-            // Fields meant for filtering, sorting
-            document.Add(new Field("PublishedDate", Package.Published.Ticks.ToString(CultureInfo.InvariantCulture), Field.Store.NO, Field.Index.NOT_ANALYZED));
-            document.Add(new Field("DownloadCount", Package.PackageRegistration.DownloadCount.ToString(CultureInfo.InvariantCulture), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            if (Package.SupportedFrameworks.AnySafe())
+            {
+                string joinedFrameworks = string.Join(";", Package.SupportedFrameworks.Select(f => f.FrameworkName));
+                document.Add(new Field("JoinedSupportedFrameworks", joinedFrameworks, Field.Store.YES,
+                                       Field.Index.NO));
+            }
 
             string displayName = String.IsNullOrEmpty(Package.Title) ? Package.PackageRegistration.Id : Package.Title;
             document.Add(new Field("DisplayName", displayName.ToLower(CultureInfo.CurrentCulture), Field.Store.NO, Field.Index.NOT_ANALYZED));
-
+           
             return document;
         }
 
