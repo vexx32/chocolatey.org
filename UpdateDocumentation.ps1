5d5358d151c171e9facfcbe653283d61550d3d1d
Rob Reynolds <ferventcoder@gmail.com>
2016-05-17 10:53:54 -0500

    Convert image urls to updated location

--- UpdateDocumentation.ps1
+++ UpdateDocumentation.ps1
@@ -14,17 +14,17 @@ function Convert-MarkdownLinks($text) {
 
   $text = $text -replace '\[\[([^\|\]]*)\|([^\|\]]*)\]\]', '<a href="$2">$1</a>'
   $text = $text -replace '\[\[([^\|\]]*)\]\]', '<a href="$1">$1</a>'
-  
+
   #if ($text -match 'href\=\"([A-Z])([^\"])+\"') {
   #  $replaceValue = 'href="' + $matches[1].ToLower() + '$2"'
   #  $text = $text.Replace( $matches[0], $replaceValue)
-  #} 
-  
+  #}
+
   Write-Output $text
 }
 
 function Fix-MarkdownConversion($text) {
-  
+
   $text = $text -creplace 'â€”', '--'
   $text = $text -creplace 'â€“', '–'
   $text = $text -creplace 'â€¦', '...'
@@ -32,9 +32,17 @@ function Fix-MarkdownConversion($text) {
   $text = $text -creplace 'â€œ', '&quot;'
   $text = $text -creplace 'â€', '&quot;'
   $text = $text -creplace 'Â', '&nbsp;'
-  
+
   Write-Output $text
 }
+
+function Convert-ImageUrls($text) {
+
+  $text = $text -replace 'img\ssrc="images\/([^"]+)"', 'img src="@Url.Content("~/content/images/docs/$1")"'
+
+  Write-Output $text
+}
+
 
 Get-ChildItem -Path choco.wiki -Recurse -ErrorAction SilentlyContinue -Filter *.md | %{
   $docName = [System.IO.Path]::GetFileNameWithoutExtension($_)
@@ -43,9 +51,10 @@ Get-ChildItem -Path choco.wiki -Recurse -ErrorAction SilentlyContinue -Filter *.
 
   & pandoc.exe --from markdown_github+simple_tables+native_spans+native_divs+multiline_tables --to html5 --old-dashes -V lang="en" -B docgen/header.txt -A docgen/footer.txt -o "$htmlFileName" "$($_.FullName)"
 
-  ($fileContent = Fix-MarkdownConversion (Convert-MarkdownLinks (Get-Content $htmlFileName).Replace("@","@@").Replace("{{AT}}","@").Replace("{{DocName}}",$docName))) | 
-    Select -Index (13..$($fileContent.count -3)) | 
+  ($fileContent = Convert-ImageUrls (Fix-MarkdownConversion (Convert-MarkdownLinks (Get-Content $htmlFileName).Replace("@","@@").Replace("{{AT}}","@").Replace("{{DocName}}",$docName)))) |
+    Select -Index (13..$($fileContent.count -3)) |
     Set-Content $htmlFileName -Force -Encoding UTF8
 }
+
 # copy the images
 Copy-Item "choco.wiki\images\*" "chocolatey\Website\content\images\docs" -Force -Recurse
