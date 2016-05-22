57f7ed35354ddbe70c9d41176976a5cf31d9e9fc
Rob Reynolds <ferventcoder@gmail.com>
2016-05-21 19:57:43 -0500

    Simply email address validation
    The original validation wasn't just crazy, but it was incorrect in some
    cases.

--- chocolatey/Website/ViewModels/EditProfileViewModel.cs
+++ chocolatey/Website/ViewModels/EditProfileViewModel.cs
@@ -26,9 +26,7 @@ namespace NuGetGallery
         [StringLength(255)]
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
-        [RegularExpression(
-            @"(?i)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$"
-            , ErrorMessage = "This doesn't appear to be a valid email address.")]
+        [RegularExpression(@"[.\S]+\@[.\S]+\.[.\S]+", ErrorMessage = "This doesn't appear to be a valid email address.")]
         public string EmailAddress { get; set; }
 
         public string PendingNewEmailAddress { get; set; }
