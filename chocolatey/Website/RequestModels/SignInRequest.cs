6a707b12277386449e50b49d4a9f90be2a5e3323
Rob Reynolds <ferventcoder@gmail.com>
2016-06-14 17:26:10 -0500

    (GH-395) Bring over code from nuget

---
+++ chocolatey/Website/RequestModels/SignInRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace NuGetGallery
+{
+    public class SignInRequest
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        [Hint("Enter your username or email address.")]
+        public string UserNameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Hint("Passwords must be at least 7 characters long.")]
+        public string Password { get; set; }
+    }
+}
