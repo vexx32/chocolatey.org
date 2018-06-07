522d8709e2accf00915cf52f469db2f8d784a604
Gary Ewan Park <gep13@gep13.co.uk>
2018-06-07 16:16:26 +0100

    (maint) Added comment for new version of pandoc
    - Some commands have been deprecated in newer versions

--- UpdateDocumentation.ps1
+++ UpdateDocumentation.ps1
@@ -106,6 +106,8 @@ Get-ChildItem -Path choco.wiki -Recurse -ErrorAction SilentlyContinue -Filter *.
   $htmlFileName = "chocolatey\Website\Views\Documentation\$($docName.Replace(`"-`", `"`")).cshtml"
 
   #+simple_tables+native_spans+native_divs+multiline_tables
+  # This is for pandoc 2.1.3 and above
+  # & pandoc.exe --from gfm+old_dashes --tab-stop=2 --to html5 --no-highlight -V lang="en" -B docgen/header.txt -A docgen/footer.txt -o "$htmlFileName" "$($_.FullName)"
   & pandoc.exe --from markdown_github --normalize --tab-stop=2 --to html5 --old-dashes --no-highlight -V lang="en" -B docgen/header.txt -A docgen/footer.txt -o "$htmlFileName" "$($_.FullName)"
 
   $fileContent = Convert-SeoUrls (Convert-ImageUrls (Convert-FencedCode (Fix-MarkdownConversion (Convert-MarkdownLinks (CleanUpHeaderIds (Get-Content $htmlFileName).Replace("@","@@").Replace("{{AT}}","@").Replace("{{DocName}}",$docName))))))
