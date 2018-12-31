3c3821c5e5d792273fa21c3f71dcf82ed1a0e278
Stephanie Burrows <designxsteph@gmail.com>
2018-12-31 07:43:08 -0600

    (layout) Redesign of Pricing page
    Redesign features the new Chocolatey layout and theme utilizing the
    bootstrap library. A new CSS file has been added for the "pages" area
    of the website, and may be later included on other pages such as the
    Documentation area or Blog.
    
    Custom.js has been modified to include smooth scrolling on anchor
    links, instead of it jumping abruptly to the designated area on the
    page.
    
    -Minor css adjustments to account.css, mostly adding
    specificity to selectors or to accommodate styles that may be used
    throughout the entire website.

--- chocolatey/Website/Scripts/custom.js
+++ chocolatey/Website/Scripts/custom.js
@@ -47,6 +47,49 @@ clipboard.on('success', function (e) {
     hideTooltip(btn);
 });
 
+// Smooth Scroll
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .not('[data-toggle="collapse"]')
+    .not('[data-toggle="tab"]')
+    .click(function (event) {
+        // Highlight active link if vertical nav
+        $(".vertical-nav").find(".active").removeClass("active");
+        $(this).parent().addClass('active');
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            var top = $('.sticky-top').outerHeight();
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1100, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+        }
+    }
+});
 // Make input text selectable with one click
 $(document).on('click', 'input[type=text]', function () {
     this.select();
