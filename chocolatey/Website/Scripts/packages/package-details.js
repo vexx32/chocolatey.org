53390158f406ef5f04ac4483e45c372d6773e685
Stephanie Burrows <designxsteph@gmail.com>
2019-06-03 08:04:42 -0500

    (layout) Adjust styling of package communication
    The package communication area is converted from markdown, and then
    styled with javascript to make it easier to read. The previous styling
    was wrapping each comment in a "li" element, which was causing problems
    if there was another "li" element inside the actual comment. This
    changes that element to a "span" element, which will no longer interfere
    with the individual comments.
    
    Minor additional addjustments made to package-details.js for
    optimization.

--- chocolatey/Website/Scripts/packages/package-details.js
+++ chocolatey/Website/Scripts/packages/package-details.js
@@ -2,15 +2,12 @@
 $('#package-save').modal('show');
 
 // Prism for Description section
-$(document).ready(function () {
-    $('#description').find("pre").addClass('line-numbers border').wrapInner('<code></code>');
-    $('#description').find("code").addClass('language-powershell');
+$(function () {
+    // Description Area
+    $('#description').find("pre").addClass('line-numbers border').wrapInner('<code class="language-powershell"></code>');
     Prism.highlightAll();
-});
 
-//Chat Bubbles
-$(document).ready(function () {
-
+    //Chat Bubbles
     // Define element containing user role
     $(".comments-list h4:contains('maintainer')").addClass("user-role");
     $(".comments-list h4:contains('reviewer')").addClass("user-role");
@@ -20,18 +17,16 @@ $(document).ready(function () {
 
     // Split user-role into two elements, so date can go on the bottom of chat bubble
     $('.user-role').each(function () {
+        // Wrap comment in span to form bubble
+        $(this).nextUntil('.user-role').addBack().wrapAll('<span></span>');
+        // Disect comment
         var $h4 = $(this),
             text = $h4.text(),
             textParts = text.split(' on');
 
-        if (textParts.length == 2) { 
+        if (textParts.length == 2) {
             $h4.text(textParts[0]).after('<h6 class="comment-date">on ' + textParts[1] + '</h6>');
         }
-    });
-
-    // Wrap in li
-    $('.user-role').each(function () {
-        $(this).nextUntil('.user-role').addBack().wrapAll('<li></li>');
     });
 
     // Unknown User
@@ -45,76 +40,76 @@ $(document).ready(function () {
     $(".comments-list.user-moderator .user-role:contains('reviewer')").parent().addClass("chat-right");
 
     // Reverse order of comments
-    ul = $('.comments-list');
-    ul.children().each(function (i, li) { ul.prepend(li) })
+    div = $('.comments-list');
+    div.children().each(function (i, span) { div.prepend(span) })
 
     // Load more comments
     var button = $(".btn-load-more");
-    $(function () {
-        if ($(".comments-list li:hidden").length < 4) {
+
+    if ($(".comments-list span:hidden").length < 4) {
+        button.hide();
+    }
+    $(".comments-list span").slice(0, 4).show().addClass('d-flex');
+    button.click(function (e) {
+        e.preventDefault();
+        $(".comments-list span:hidden").slice(0, 100).show().addClass('d-flex');
+        if ($(".comments-list span:hidden").length == 0) {
             button.hide();
         }
-        $(".comments-list li").slice(0, 4).show().addClass('d-flex');
-        button.click(function (e) {
-            e.preventDefault();
-            $(".comments-list li:hidden").slice(0, 100).show().addClass('d-flex');
-            if ($(".comments-list li:hidden").length == 0) {
-                button.hide();
-            }
-        });
     });
-});
 
-// Replace Show/Hide on buttons containing more than 1 word when clicked
-$('.btn').click(function () {
-    var $this = $(this);
-    if ($this.is(':contains("Show")')) {
-        $this.each(function () {
-            var text = $this.text().replace('Show', 'Hide');
-            $this.text(text);
-        });
-    } else if ($this.is(':contains("Hide")')) {
-        $this.each(function () {
-            var text = $this.text().replace('Hide', 'Show');
-            $this.text(text);
-        });
-    }
-});
+    // Files Section
+    // Replace Show/Hide on buttons when clicked
+    $('.btn').click(function () {
+        var $this = $(this);
+        if ($this.is(':contains("Show")')) {
+            $this.each(function () {
+                var text = $this.text().replace('Show', 'Hide');
+                $this.text(text);
+            });
+        } else if ($this.is(':contains("Hide")')) {
+            $this.each(function () {
+                var text = $this.text().replace('Hide', 'Show');
+                $this.text(text);
+            });
+        }
+    });
 
-// Expand or Show all files
-$('#files .btn-danger').click(function () {
-    var $this = $(this);
-    $this.toggleClass('btn-success');
-    if ($this.hasClass('btn-success')) {
-        $this.html('<i class="icon-plus-circle" alt="Show Files"></i> Expand All');
-        $this.removeClass('btn-danger');
-        $(".collapse-2-content").removeClass('d-block').addClass('d-none').find("pre").removeClass('line-numbers language-powershell').find("code").removeClass('language-powershell');
-        var $this = $('#files .btn:contains("Hide")');
-        $this.html('Show');
-    } else {
-        $this.html('<i class="icon-minus-circle" alt="Collapse Files"></i> Collapse Files');
-        $this.removeClass('btn-success').addClass('btn-danger');
-        $(".collapse-2-content").removeClass('d-none').addClass('d-block').find("pre").addClass('line-numbers language-powershell').find("code").addClass('language-powershell');
-        var $this = $('#files .btn:contains("Show")');
-        $this.html('Hide');
-    }
-});
+    // Expand or Show all files
+    $('#files .btn-danger').click(function () {
+        var $this = $(this);
+        $this.toggleClass('btn-success');
+        if ($this.hasClass('btn-success')) {
+            $this.html('<i class="icon-plus-circle" alt="Show Files"></i> Expand All');
+            $this.removeClass('btn-danger');
+            $(".collapse-2-content").removeClass('d-block').addClass('d-none').find("pre").children().removeClass().children().removeClass().children().removeClass();
+            var $this = $('#files .btn:contains("Hide")');
+            $this.html('Show');
+        } else {
+            $this.html('<i class="icon-minus-circle" alt="Collapse Files"></i> Collapse Files');
+            $this.removeClass('btn-success').addClass('btn-danger');
+            $(".collapse-2-content").removeClass('d-none').addClass('d-block').find("pre").addClass('line-numbers language-powershell').find("code").addClass('language-powershell');
+            var $this = $('#files .btn:contains("Show")');
+            $this.html('Hide');
+        }
+    });
 
-// Approved packages
-$('.collapse-2 .btn').click(function () {
-    var $this = $(this).nextAll(".collapse-2-content");
-    $this.toggleClass('d-none');
-    if ($this.hasClass('d-none')) {
-        $this.removeClass('d-block').find("pre").removeClass('line-numbers language-powershell').find("code").removeClass('language-powershell');
-    } else {
-        $this.removeClass('d-none').addClass('d-block').find("pre").addClass('line-numbers language-powershell').find("code").addClass('language-powershell');
-        Prism.highlightAll();
-    }
-});
-// If expanded for moderation
-var $files = $('.moderation-view .collapse-2');
-var $expand = $(".moderation-view .collapse-2:contains('.nuspec'), .moderation-view .collapse-2:contains('chocolateyinstall.ps1'), .moderation-view .collapse-2:contains('chocolateyuninstall.ps1'), .moderation-view .collapse-2:contains('chocolateybeforemodify.ps1'), .moderation-view .collapse-2:contains('verification.txt'), .moderation-view .collapse-2:contains('license.txt')");
-$(document).ready(function () {
+    // Approved packages
+    $('.collapse-2 .btn').click(function () {
+        var $this = $(this).nextAll(".collapse-2-content");
+        $this.toggleClass('d-none');
+        if ($this.hasClass('d-none')) {
+            $this.removeClass('d-block').find("pre").children().removeClass().children().removeClass().children().removeClass();
+        } else {
+            $this.removeClass('d-none').addClass('d-block').find("pre").addClass('line-numbers language-powershell').find("code").addClass('language-powershell');
+            Prism.highlightAll();
+        }
+    });
+
+    // If expanded for moderation
+    var $files = $('.moderation-view .collapse-2');
+    var $expand = $(".moderation-view .collapse-2:contains('.nuspec'), .moderation-view .collapse-2:contains('chocolateyinstall.ps1'), .moderation-view .collapse-2:contains('chocolateyuninstall.ps1'), .moderation-view .collapse-2:contains('chocolateybeforemodify.ps1'), .moderation-view .collapse-2:contains('verification.txt'), .moderation-view .collapse-2:contains('license.txt')");
+
     // Expand Files 
     if ($files.length <= 9) {
         $('.moderation-view .collapse-2-content').addClass('d-block').removeClass('d-none');
