7ad16cbd729a2854a7a105875d66ffd2ea5076af
Stephanie Burrows <designxsteph@gmail.com>
2019-10-07 08:15:28 -0500

    (layout) Ensure anchors are added to URL on click
    A line in the smooth scroll javascript was preventing the anchor
    clicked within a page from being added to the URL. With the removal of
    the snippet of javascript in this commit, the anchor hash is now added
    to the URL, making it easy to copy/paste to give to another user.

--- chocolatey/Website/Scripts/custom.js
+++ chocolatey/Website/Scripts/custom.js
@@ -186,8 +186,6 @@ $('a[href*="#"]')
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             // Does a scroll target exist?
             if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
                 }, 1100, function () {
