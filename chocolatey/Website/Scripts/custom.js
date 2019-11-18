941a801ecb1a6a39b4402b99161c4245bc011274
Stephanie Burrows <designxsteph@gmail.com>
2019-11-18 10:28:12 -0600

    (maint) Move CookiesNotice.js into custom.js
    The CookiesNotice.js was being referenced individually on every single
    page, and a js snippet was also being referenced inline on the main
    layout. The CookiesNotice js and inline code snippet have both been
    moved to custom.js which is mentioned on every page.

--- chocolatey/Website/Scripts/custom.js
+++ chocolatey/Website/Scripts/custom.js
@@ -8,6 +8,26 @@ $(window).on('load', function () {
 // Show modal on tempdata "message"
 $('#tempdata-message').modal('show');
 
+// Cookies Notice
+var CookiesNotice = (function () {
+    return {
+        modal: function (cookieName) {
+            $(".cookies-popup .cookies-close").click(function (e) {
+                $(e.target).closest(".cookies-popup").hide();
+                var d = new Date();
+                // 100 years in milliseconds: 100 years * 365 days * 24 hours * 60 minutes * 60 seconds * 1000ms
+                d.setTime(d.getTime() + (100 * 365 * 24 * 60 * 60 * 1000));
+                var expires = "expires=" + d.toUTCString();
+                document.cookie = cookieName + "=true;" + expires + ";path=/";
+            });
+        }
+    }
+})();
+CookiesNotice.modal("chocolatey_hide_cookies_notice");
+if (!getCookie('chocolatey_hide_cookies_notice')) {
+    $(".cookies-popup").css('display', 'block');
+}
+
 // Top Navigation
 $(document).ready(function () {
     // Top Alert
