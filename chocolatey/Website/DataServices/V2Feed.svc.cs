303c52c6143640c1daa9398d1ad8618a6d0b0ec0
Rob Reynolds <ferventcoder@gmail.com>
2019-12-16 22:49:20 -0600

    (GH-765) FindPackagesById uses old method for now
    While it is worked out on what it causing high latency, switch back to
    old  method, allowing other enhancements and fixes to continue to be
    applied.

--- chocolatey/Website/DataServices/V2Feed.svc.cs
+++ chocolatey/Website/DataServices/V2Feed.svc.cs
@@ -153,29 +153,29 @@ namespace NuGetGallery
                             .Include(p => p.PackageRegistration)
                             .Where(p => p.PackageRegistration.Id.Equals(id) && (p.StatusForDatabase != _rejectedStatus || p.StatusForDatabase == null));
 
-            if (searchService.ContainsAllVersions)
-            {
-                return NugetGallery.Cache.Get(
-                    string.Format("V2Feed-FindPackagesById-{0}", id.to_lower()),
-                    DateTime.UtcNow.AddSeconds(DEFAULT_CACHE_TIME_SECONDS_V2FEED),
-                    () =>
-                    {
-                        var searchFilter = GetSearchFilter(searchService.ContainsAllVersions, HttpContext.Request.RawUrl, id, includePrerelease: true);
-                        // Find packages by Id specific items
-                        searchFilter.ExactIdOnly = true;
-                        searchFilter.TakeAllResults = true;
-                        searchFilter.SortProperty = SortProperty.Version;
-                        searchFilter.SortDirection = SortDirection.Descending;
+            //if (searchService.ContainsAllVersions)
+            //{
+            //    return NugetGallery.Cache.Get(
+            //        string.Format("V2Feed-FindPackagesById-{0}", id.to_lower()),
+            //        DateTime.UtcNow.AddSeconds(DEFAULT_CACHE_TIME_SECONDS_V2FEED),
+            //        () =>
+            //        {
+            //            var searchFilter = GetSearchFilter(searchService.ContainsAllVersions, HttpContext.Request.RawUrl, id, includePrerelease: true);
+            //            // Find packages by Id specific items
+            //            searchFilter.ExactIdOnly = true;
+            //            searchFilter.TakeAllResults = true;
+            //            searchFilter.SortProperty = SortProperty.Version;
+            //            searchFilter.SortDirection = SortDirection.Descending;
 
-                        return SearchCore(packages, id, string.Empty, includePrerelease: true, searchFilter: searchFilter, useCache: true)
-                            .ToV2FeedPackageQuery(GetSiteRoot())
-                            .ToList();
-                    }).AsQueryable();
-            }
+            //            return SearchCore(packages, id, string.Empty, includePrerelease: true, searchFilter: searchFilter, useCache: true)
+            //                .ToV2FeedPackageQuery(GetSiteRoot())
+            //                .ToList();
+            //        }).AsQueryable();
+            //}
 
             return NugetGallery.Cache.Get(
                 string.Format("V2Feed-FindPackagesById-{0}", id.to_lower()),
-                DateTime.UtcNow.AddSeconds(DEFAULT_CACHE_TIME_SECONDS_V2FEED),
+                DateTime.UtcNow.AddSeconds(DEFAULT_CACHE_TIME_SECONDS_V2FEED * 3),
                 () => packages
                         .ToV2FeedPackageQuery(GetSiteRoot())
                         .ToList()
