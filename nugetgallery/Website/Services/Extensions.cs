5104c270d7382a45360d47ed3dbe29794b128ef5
Rob Reynolds <ferventcoder@gmail.com>
2019-12-30 16:34:21 -0600

    (GH-785) Remove ToLower for database queries
    When querying the database, their is no need to convert to lower as the
    default SQL server settings is to be case insensitive.

--- nugetgallery/Website/Services/Extensions.cs
+++ nugetgallery/Website/Services/Extensions.cs
@@ -9,19 +9,19 @@ namespace NuGetGallery
     {
         // Search criteria
         private static Func<string, Expression<Func<Package, bool>>> idCriteria = term =>
-            p => p.PackageRegistration.Id.ToLower().Contains(term);
+            p => p.PackageRegistration.Id.Contains(term);
 
         private static Func<string, Expression<Func<Package, bool>>> descriptionCriteria = term =>
-            p => p.Description.ToLower().Contains(term);
+            p => p.Description.Contains(term);
 
         private static Func<string, Expression<Func<Package, bool>>> summaryCriteria = term =>
-            p => p.Summary != null && p.Summary.ToLower().Contains(term);
+            p => p.Summary != null && p.Summary.Contains(term);
 
         private static Func<string, Expression<Func<Package, bool>>> tagCriteria = term =>
-            p => p.Tags != null && p.Tags.ToLower().Contains(term);
+            p => p.Tags != null && p.Tags.Contains(term);
 
         private static Func<string, Expression<Func<Package, bool>>> authorCriteria = term =>
-            p => p.Authors.Any(a => a.Name.ToLower().Contains(term));
+            p => p.Authors.Any(a => a.Name.Contains(term));
 
         private static Func<string, Expression<Func<Package, bool>>>[] searchCriteria = new[] { 
                 idCriteria, 
@@ -43,7 +43,7 @@ namespace NuGetGallery
             var expressions = new List<LambdaExpression>();
             foreach (var term in terms)
             {
-                var localSearchTerm = term.to_lower();
+                var localSearchTerm = term.to_string();
 
                 if (localSearchTerm.StartsWith("id:"))
                 {
