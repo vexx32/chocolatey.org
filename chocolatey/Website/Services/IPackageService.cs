a0964d926c068085d525636dd525e3da394a4759
Gary Ewan Park <gep13@gep13.co.uk>
2020-06-29 15:38:16 +0100

    rebase onto last commit

--- chocolatey/Website/Services/IPackageService.cs
+++ chocolatey/Website/Services/IPackageService.cs
@@ -1,15 +1,15 @@
-﻿// Copyright 2011 - Present RealDimensions Software, LLC, the original 
+﻿// Copyright 2011 - Present RealDimensions Software, LLC, the original
 // authors/contributors from ChocolateyGallery
 // at https://github.com/chocolatey/chocolatey.org,
-// and the authors/contributors of NuGetGallery 
+// and the authors/contributors of NuGetGallery
 // at https://github.com/NuGet/NuGetGallery
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,10 +29,13 @@ namespace NuGetGallery
         void DeletePackage(string id, string version);
 
         PackageRegistration FindPackageRegistrationById(string id);
+
         PackageRegistration FindPackageRegistrationById(string id, bool useCache);
 
         Package FindPackageByIdAndVersion(string id, string version, bool allowPrerelease = true);
+
         Package FindPackageByIdAndVersion(string id, string version, bool allowPrerelease, bool useCache = true);
+
         Package FindPackageForDownloadByIdAndVersion(string id, string version, bool allowPrerelease, bool useCache = true);
 
         IQueryable<Package> GetPackagesForListing(bool includePrerelease);
